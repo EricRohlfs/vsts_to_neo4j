50919bf4a8054f16bfaf6f69e273037f88120004
Eric Rohlfs <ericghent@gmail.com>
2019-05-02 15:05:42 -0400

    smarter scripts for cloning missing projects only

--- code/SyncRepos.py
+++ code/SyncRepos.py
@@ -6,6 +6,8 @@ from multiprocessing import Pool
 from ProjectsTeamsUsers import ProjectsTeamsUsersWorker
 from Repositories import RepositoriesWorker
 from VSTSInfo import VstsInfo
+import configparser
+import os
 
 class RepoSync(object):
     """
@@ -39,25 +41,70 @@ class RepoSync(object):
             changed.append(url.replace(old_fqdn, new_fqdn))
         return changed
 
-    def make_clone_script(self, repo_list):
+    def make_clone_script(self, repo_list, missing, git_folder_root):
         """
         Makes a clone script to run in a batch file
         """
         clone = []
         for url in repo_list:
-            cmd = "git clone {0}".format(url)
-            clone.append(cmd)
+            repo_name = self.get_repo_name_from_url(url)
+            for missed in missing:
+                if missed in url:
+                    #where_to_clone = "{0}{1}\\".format(git_folder_root, repo_name)
+                    where_to_clone = os.path.join(git_folder_root, repo_name)
+                    cmd = 'git clone {0} \"{1}\"'.format(url, where_to_clone)
+                    clone.append(cmd)
         return clone
+
+    def get_local_git_repos(self, git_root):
+        """
+        Crawls a folder, gets the list of folder names
+        The folder names should be the same as the repo names
+        """
+        #repos = [x[0] for x in os.walk(git_root)]
+        repos = os.listdir(git_root) 
+        return repos
+
+    def get_repo_name_from_url(self, url):
+        """
+        given a git clone url, get the name of the repo
+        """
+        full = len(url)
+        idx = url.find("_git/") + 5
+        name = url[idx:full]
+        return name
+
+
+    def get_missing_local_repos(self, local_folders, repos):
+        """
+        Compares the list of repo names to local folders and returns the difference 
+        """
+        repo_names = []
+        for url in repos:
+            name = self.get_repo_name_from_url(url)
+            repo_names.append(name)
+
+        missing = set(repo_names).difference(local_folders)
+        return missing
+
+    #def generate_remotes(self, )
 
 
 if __name__ == '__main__':
     print("starting Repo Sync")
-    #set to false for easier debugging, but it is slower
-    RUN_MULTITHREADED = False
-    OLD_FQDN = "https://innovasystems.visualstudio.com"
-    NEW_FQDN = "https://example.com"
 
-    VSTS = VstsInfo(None, None, ignore_cache=True)
+    config = configparser.ConfigParser()
+    config.read_file(open('default.cfg'))
+
+    #set to false for easier debugging, but it is slower
+    RUN_MULTITHREADED = config['RepoSync']['RunMultiThreaded']
+    RUN_MULTITHREADED = RUN_MULTITHREADED in ['True', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+    SERVER_NO_IP =  config['RepoSync']['ServerNoIp']
+    SERVER_IP = config['RepoSync']['ServerIp']
+    GIT_ROOT_FOLDER_PATH = config['RepoSync']['GitRootFolderPath']
+
+    #for this script we always want to ignore the cache
+    VSTS = VstsInfo(None, None, ignore_cache=False)
 
     PTU_WORKER = ProjectsTeamsUsersWorker(VSTS.get_request_settings(), VSTS.project_whitelist, VSTS)
     PROJECTS_URL = PTU_WORKER.get_vsts_projects_url()
@@ -67,24 +114,27 @@ if __name__ == '__main__':
     REPO_WORKER = RepositoriesWorker(VSTS.get_request_settings(), VSTS)
 
     REPO_SYNC = RepoSync()
+    LOCAL_REPOS = REPO_SYNC.get_local_git_repos(GIT_ROOT_FOLDER_PATH)
 
-    GIT_WEB = []
+    REPO_NOIP_STORE = []
 
     if RUN_MULTITHREADED:
         with Pool(5) as p:
-            p.map(REPO_SYNC.add_repo_urls_to_store, PROJECTS, GIT_WEB)
+            p.map(REPO_SYNC.add_repo_urls_to_store, PROJECTS, REPO_NOIP_STORE)
     else:
         for PROJ in PROJECTS:
-            REPO_SYNC.add_repo_urls_to_store(PROJ, GIT_WEB)
-    
-    REPO_SYNC.save_repo_urls(GIT_WEB, "git_web_urls_original.txt")
+            REPO_SYNC.add_repo_urls_to_store(PROJ, REPO_NOIP_STORE)
 
-    GIT_WEB2 = REPO_SYNC.swap_fqdn(GIT_WEB, OLD_FQDN, NEW_FQDN)
-    REPO_SYNC.save_repo_urls(GIT_WEB2, "git_web_urls.txt")
 
-    CLONE = REPO_SYNC.make_clone_script(GIT_WEB2)
-    REPO_SYNC.save_repo_urls(CLONE, "git_clone_external.bat")
+    LOCAL_MISSING = REPO_SYNC.get_missing_local_repos(LOCAL_REPOS, REPO_NOIP_STORE)
+    CLONE_MISSING = REPO_SYNC.make_clone_script(REPO_NOIP_STORE, LOCAL_MISSING, GIT_ROOT_FOLDER_PATH)
+    REPO_SYNC.save_repo_urls(CLONE_MISSING, GIT_ROOT_FOLDER_PATH + "\\git_clone_missing.bat")
 
+
+    #REPO_SYNC.save_repo_urls(REPO_NOIP_STORE, "git_web_urls_original.txt")
+
+    #REPO_IP_STORE = REPO_SYNC.swap_fqdn(REPO_NOIP_STORE, SERVER_NO_IP, SERVER_IP)
+    #REPO_SYNC.save_repo_urls(REPO_IP_STORE, "git_web_urls.txt")
 
 
 
